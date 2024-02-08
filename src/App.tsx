068acc3ab2fed06835979a249a39783b7f05b47c
shim506 <shim506@naver.com>
2024-02-08 18:35:12 +0900

    feat: git init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a>
+          지유나 gather 앱 만들 준비 됐어?
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
